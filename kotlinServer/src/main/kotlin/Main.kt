34c2c014eb37058d5eea1a1b5b1b96e5f01e504e
Jörgen Andersson <jorgen@kollektiva.se>
2018-01-23 09:39:32 +0100

    Comments

--- kotlinServer/src/main/kotlin/Main.kt
+++ kotlinServer/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 /**
- * Purpose of this class ...
+ * Purpose of this class is main method for starting Spark WebServer
+ * For more info about Spark(Java), please see http://sparkjava.com/documentation#views-and-templates
  *
  * Created by Jorgen Andersson (jorgen@kollektiva.se) on 2018-01-21.
  */
@@ -8,16 +9,21 @@
 //Main.kt
 import spark.Spark.*
 
+/**
+ * Application main
+ */
 fun main(args: Array<String>) {
-
+    //
     // File server
     spark.kotlin.staticFiles.location("/site")
-    // Rest server
-    get("/hello") { req, res -> "Hello World" }
     //
-    path("/users") {
-        get("") {req, res -> "All users"}
-        get("/:id") {req, res -> "User with id: " + req.params("id")}
+    // Rest server
+    path("/api") {
+        get("/hello") { req, res -> "Hello World" }
+        //
+        path("/users") {
+            get("") {req, res -> "All users"}
+            get("/:id") {req, res -> "User with id: " + req.params("id")}
+        }
     }
-
 }
