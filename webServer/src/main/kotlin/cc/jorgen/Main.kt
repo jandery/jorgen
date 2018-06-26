800ae4727ed6ea9a3f695cc280280dbc52166bc9
Jörgen Andersson <jorgen@kollektiva.se>
2018-06-26 14:21:12 +0200

    comments

--- webServer/src/main/kotlin/cc/jorgen/Main.kt
+++ webServer/src/main/kotlin/cc/jorgen/Main.kt
@@ -5,7 +5,7 @@ import io.schinzel.basicutils.configvar.ConfigVar
 import io.schinzel.basicutils.configvar.IConfigVar
 
 /**
- * Purpose of this file is ...
+ * Purpose of this file is to start a WebServer
  *
  * Created by Jorgen Andersson on 2018-06-26.
  */
