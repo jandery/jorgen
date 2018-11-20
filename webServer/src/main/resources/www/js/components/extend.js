3f0b71187dad785176ad5558ce51f84b29e9f8fb
Jörgen Andersson <jorgen@kollektiva.se>
2018-11-20 13:57:17 +0100

    Playing with extends

---
+++ webServer/src/main/resources/www/js/components/extend.js
@@ -0,0 +1,54 @@
+
+
+class JorgenBucket extends HTMLElement {
+    constructor() {
+        super();
+        this.root = this.attachShadow({mode: "open"});
+    }
+
+    connectedCallback() {
+        console.log('jorgen-bucket connect');
+        this.render();
+    }
+
+    render() {
+        this.root.innerHTML = `
+            <div>
+                <label>Förnamn<input type="text" value="${this.firstName}"/></label>
+                <label>Efternamn<input type="text" value="${this.lastName}"/></label>
+                <label>Ålder<input type="text" value="${this.age}"/></label>
+            </div>
+        `;
+    }
+
+    setData(cls) {
+        console.log("jorgen-bucket setting data");
+        this.data = cls;
+        this.render();
+    }
+
+    getData() {
+        let inputs = this.shadowRoot.querySelectorAll("input");
+        console.log("There are " + inputs.length + " inputs in element");
+    }
+
+    get firstName() {
+        return this.data ? this.data.firstName : "";
+    }
+
+    get lastName() {
+        return this.data ? this.data.lastName : "";
+    }
+
+    get age() {
+        return this.data ? this.data.age : "";
+
+    }
+
+}
+
+
+if (!window.customElements.get('jorgen-bucket')) {
+    customElements.whenDefined('jorgen-bucket').then(() => { console.log('jorgen-bucket defined'); });
+    customElements.define("jorgen-bucket", JorgenBucket);
+}
